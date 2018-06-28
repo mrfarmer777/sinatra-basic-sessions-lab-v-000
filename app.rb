724198fa223261d66387b357ae990c5f9a1afa63
Matt Farmer <mrfarmer777@gmail.com>
2018-06-28 16:25:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  #enabling sessions and setting the secret
   configure do
     enable :sessions
     set :session_secret, "pickles"
   end
-  
-  
+
+
 end
