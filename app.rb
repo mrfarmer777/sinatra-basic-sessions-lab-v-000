b8b0ab982bfd670c8f4672bfea5e6b51e04597a5
Matt Farmer <mrfarmer777@gmail.com>
2018-06-28 16:31:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  #enabling sessions and setting the secret
+  configure do
+    enable :sessions
+    set :session_secret, "pickles"
+  end
+
+  get "/" do
+    erb :index
+  end
+
+  post "/checkout" do
+    @session=session
+    @session[:item]=params[:item]
+  end
 end
